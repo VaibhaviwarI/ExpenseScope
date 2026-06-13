3f80efd6020f96761f90213fb28afbd02a779ece
vaibhav <vaibhavtiwari81356@gmail.com>
2026-06-14 00:03:46 +0530

    Using common js instead of import

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("ExpenseScope API Running");
+app.get("/", (req, res) => {
+  res.send("ExpenseScope API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
